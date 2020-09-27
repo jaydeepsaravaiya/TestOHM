bccfce26b0e6fe5d36ecdf8d97a8863fedc923dd
JD Saravaiya <39241212+jaydeepsaravaiya@users.noreply.github.com>
2020-09-28 02:30:31 +0530

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CPUFrequencyWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(775, 380);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(713, 415);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Management;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenHardwareMonitor.Hardware;
+
+using Timer = System.Windows.Forms.Timer;
+
+namespace CPUFrequencyWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Timer timer;
+        Computer computer = new Computer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new Timer()
+            {
+                Interval = 100,
+                Enabled = true
+            };
+            var task = new Task(TaskWork);
+
+            task.Start();
+            timer.Tick += (o, ea) =>
+            {
+                timer.Stop();
+                    if (task.Status == TaskStatus.RanToCompletion)
+                    {
+                        task = new Task(TaskWork);
+                        task.Start();
+                    }
+                timer.Start();
+            };
+            
+        }
+
+        private void TaskWork()
+        {
+                var cpuInfo = GetCPUInfoOHM();
+                if (InvokeRequired)
+                {
+                    Invoke(new Action(() => { richTextBox1.Text = cpuInfo; }));
+                }
+                else
+                {
+                    richTextBox1.Text = cpuInfo;
+                }
+        }
+
+        private static string GetCPUInfo()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter("Processor Information", "% Processor Performance", "_Total");
+            double cpuValue = cpuCounter.NextValue();
+
+            foreach (ManagementObject obj in new ManagementObjectSearcher("SELECT *, Name FROM Win32_Processor").Get())
+            {
+                double maxSpeed = Convert.ToDouble(obj["MaxClockSpeed"]) / 1000;
+                double turboSpeed = maxSpeed * cpuValue / 100;
+                return string.Format("{0} Running at {1:0.00}Ghz, Turbo Speed: {2:0.00}Ghz", obj["Name"], maxSpeed, turboSpeed);
+            }
+
+            return string.Empty;
+        }
+        private string GetCPUInfoOHM()
+        {
+            computer.CPUEnabled = true;
+            computer.Open();
+
+            string returnValue="";
+            foreach (var hardware in computer.Hardware)
+            {
+                if(hardware.HardwareType == HardwareType.CPU)
+                {
+                    var cpu = hardware;
+                    cpu.Update();
+                    foreach(var sensor in cpu.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Clock)
+                        {
+                            returnValue += string.Format("[{2}] {0}=>{1}\n",sensor.Name, sensor.Value,DateTime.UtcNow);
+                        }
+                    }
+                }
+            }
+            return returnValue;
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
